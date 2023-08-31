09d7bdd7cb21e1668bc26d20f4464747429c7627
ver <luzverde0314@gmail.com>
2023-08-31 23:55:26 +0900

    연

---
+++ first/src/first/HelloWorld.java
@@ -0,0 +1,15 @@
+package first;
+
+public class HelloWorld {
+//	프로그램의 시작점
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+/*		System.out.println("Hello World2");
+		System.out.println("Hello World3"); */
+	}
+
+}
